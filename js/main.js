2c6007c79f8c201eb4ed9cb4e1d3ce62be6b4b09
k-marczak <k_marczak@outlook.com>
2020-01-17 14:56:03 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-$('.slider-one').slick();
+window.sr= ScrollReveal();
+sr.reveal('.container',{duration:1000});
+sr.reveal('.container-fluid',{duration:1000});
+sr.reveal('.row',{duration:1000});
+sr.reveal('.cols3',{duration:1000});
